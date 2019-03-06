f8618010eb50233355581a43078b8c70681b92c4
Yuan Gao <y1gao@ucsd.edu>
2019-03-06 15:42:36 -0800

    Update RelationExplorer.py

--- Utils/RelationExplorer.py
+++ Utils/RelationExplorer.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-class RelationExplorer():
+class RelationExplorer:
     """
     Visualize the relation(positive/negative/none) between two rows
     """
